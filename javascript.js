fcfdf437a55ca1fe64b0501d4d0cabf28a000e2a
Kêmele Clara Alves Mendonça <116192157+KemeleClara@users.noreply.github.com>
2023-05-23 16:34:38 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+ 
+function ativarNome(){
+    let nome = 'Sávio best teacher in the world '
+    console.log(nome)
+    document.getElementById('aparecer').innerHTML = nome
+}
+
+
+function somarNumeros(){
+    let a = 5;
+    let b = 10;
+    let c = a+b;
+    console.log(a,b,c)
+document.getElementById('aparecer').innerHTML = c
+
+}
+
+
+
+
+
+
